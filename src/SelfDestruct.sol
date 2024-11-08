30bde7dfb07d0494f78a73304e53c0f93a75ca91
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-11-08 14:53:20 -0800

    Update SelfDestruct.sol

--- src/SelfDestruct.sol
+++ src/SelfDestruct.sol
@@ -1,1 +1,24 @@
 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract SelfDestruct {
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function unprotected() public {
+        selfdestruct(payable(msg.sender));
+    }
+
+    function unprotected2() external {
+        suicide(owner);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+}
